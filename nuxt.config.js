ba94bab1aba33b1424edd24243d4b93e73d8d3dc
ujez <ujezujez@gmail.com>
2022-02-16 14:11:06 +0100

    added some pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/vue-placeholders.js',
+    '~/plugins/vue-observe-visibility.client.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
